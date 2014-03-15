daaa663c3eb562edc30c3e38ae79e53765a3d468
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-03-14 20:44:27 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DumbStore::Application.config.secret_key_base = 'b9c33f1c72e6988627bbd3a54d550b199c44b3382cc506db41faad832d8ea73eb78077e5174638fc3fafce1f662306b06a74a161183b633c3235648c7766bb3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DumbStore::Application.config.session_store :cookie_store, key: '_dumb_store_session'
